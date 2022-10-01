48fb8bb380cade8a13a13bd592f64747e4185c18
Johan Rönkkö <ronkko.johan@gmail.com>
2022-10-01 21:55:13 +0200

    Remove todos

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -30,22 +30,15 @@ require "plugin.settings.dap"
 require "plugin.settings.whichkey"
 
 -- Inspo:
--- https://github.com/nvim-telescope/telescope-dap.nvim and dap plugin
 -- https://github.com/nvim-neo-tree/neo-tree.nvim (see https://github.com/Allaman/nvim/blob/main/lua/plugins.lua)
 
--- https://github.com/Allaman/nvim/blob/main/lua/plugins.lua
--- https://github.com/tpope/vim-fugitive
 -- folke/todo-comments.nvim
 -- https://github.com/EdenEast/nightfox.nvim
--- https://github.com/mfussenegger/nvim-dap check what dap is used in lunarvim
--- for above, check out https://github.com/Allaman/nvim/blob/main/lua/plugins.lua
 -- https://github.com/lukas-reineke/headlines.nvim
 -- https://github.com/kevinhwang91/nvim-bqf
 
 -- TODO:
 -- * make config unbreakable with commit hash in packer use statement.
--- * tab vs spaces in Go? after/ftdetect file with config specific for go.lua?
 -- * colorscheme. Backticks in markdown files in nord theme?
--- * dap debugger
 -- * learn lazygit properly
 -- * get typescript/react prettier/eslintd setup working
