254ee8606a5b76a4dad5f4f7236465b1989a0917
Johan Rönkkö <ronkko.johan@gmail.com>
2023-05-04 22:57:57 +0200

    Format wezterm lua code.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,34 +1,33 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local act = wezterm.action
-local gui = wezterm.gui
 
 local config = {}
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- OpenGL for GPU acceleration, Software for CPU
-config.front_end = "OpenGL"
+config.front_end = 'OpenGL'
 
 ------------------------------------
 -- Events
 ------------------------------------
 
-function scandir(directory)
-    local pfile = assert(io.popen(("find '%s' -mindepth 1 -maxdepth 1 -type d -printf '%%f\\0'"):format(directory), 'r'))
-    local list = pfile:read('*a')
-    pfile:close()
+local function scandir(directory)
+  local pfile = assert(io.popen(("find '%s' -mindepth 1 -maxdepth 1 -type d -printf '%%f\\0'"):format(directory), 'r'))
+  local list = pfile:read '*a'
+  pfile:close()
 
-    local folders = {}
-    for filename in string.gmatch(list, '[^%z]+') do
-        table.insert(folders, filename)
-    end
+  local folders = {}
+  for filename in string.gmatch(list, '[^%z]+') do
+    table.insert(folders, filename)
+  end
 
-    return folders
+  return folders
 end
 
-wezterm.on('update-right-status', function(window, pane)
+wezterm.on('update-right-status', function(window, _)
   window:set_right_status(window:active_workspace())
 end)
 
@@ -51,9 +50,9 @@ end)
 -- Colors
 config.color_scheme = 'GitHub Dark'
 config.color_schemes = {
-	['plain_white'] = require('color_schemes.plain_white'),
-	['plain_black'] = require('color_schemes.plain_black'),
-	['boring'] = require('color_schemes.boring'),
+  ['plain_white'] = require 'color_schemes.plain_white',
+  ['plain_black'] = require 'color_schemes.plain_black',
+  ['boring'] = require 'color_schemes.boring',
 }
 
 -- Transparency
@@ -63,13 +62,13 @@ config.line_height = 1.0
 config.dpi = 96.0
 
 -- Padding
-local horizontalPadding = 5;
-local verticalPadding = 5;
+local horizontalPadding = 5
+local verticalPadding = 5
 config.window_padding = {
-	left = horizontalPadding,
-	right = horizontalPadding,
-	top = verticalPadding,
-	bottom = verticalPadding,
+  left = horizontalPadding,
+  right = horizontalPadding,
+  top = verticalPadding,
+  bottom = verticalPadding,
 }
 
 -- Tab bar
@@ -91,52 +90,52 @@ config.skip_close_confirmation_for_processes_named = {}
 config.disable_default_key_bindings = true
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
-	-- Leader fix: Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A.
-	{ key = 'a', mods = 'LEADER|CTRL',  action = act.SendString '\x01' },
-	-- Move between panes.
-	{ key = 'h', mods = 'LEADER',       action = act.ActivatePaneDirection 'Left' },
-	{ key = 'l', mods = 'LEADER',       action = act.ActivatePaneDirection 'Right' },
-	{ key = 'k', mods = 'LEADER',       action = act.ActivatePaneDirection 'Up' },
-	{ key = 'j', mods = 'LEADER',       action = act.ActivatePaneDirection 'Down' },
-  { key = 's', mods = 'LEADER',       action = act.PaneSelect },	
+  -- Leader fix: Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A.
+  { key = 'a', mods = 'LEADER|CTRL', action = act.SendString '\x01' },
+  -- Move between panes.
+  { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection 'Left' },
+  { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection 'Right' },
+  { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection 'Up' },
+  { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection 'Down' },
+  { key = 's', mods = 'LEADER', action = act.PaneSelect },
   -- Create/close panes.
-	{ key = '"', mods = 'LEADER|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-	{ key = '%', mods = 'LEADER|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-	{ key = 'x', mods = 'LEADER',       action = act.CloseCurrentPane { confirm = true } },
-	-- Move between tabs.
-	{ key = '1', mods = 'LEADER',       action = act.MoveTab(1 - 1) },
-	{ key = '1', mods = 'LEADER',       action = act.ActivateTab(1 - 1) },
-	{ key = '2', mods = 'LEADER',       action = act.MoveTab(2 - 1) },
-	{ key = '2', mods = 'LEADER',       action = act.ActivateTab(2 - 1) },
-	{ key = '3', mods = 'LEADER',       action = act.MoveTab(3 - 1) },
-	{ key = '3', mods = 'LEADER',       action = act.ActivateTab(3 - 1) },
-	{ key = '4', mods = 'LEADER',       action = act.MoveTab(3 - 1) },
-	{ key = '4', mods = 'LEADER',       action = act.ActivateTab(4 - 1) },
-	{ key = '5', mods = 'LEADER',       action = act.MoveTab(5 - 1) },
-	{ key = '5', mods = 'LEADER',       action = act.ActivateTab(5 - 1) },
-	{ key = '6', mods = 'LEADER',       action = act.MoveTab(6 - 1) },
-	{ key = '6', mods = 'LEADER',       action = act.ActivateTab(6 - 1) },
-	{ key = '7', mods = 'LEADER',       action = act.MoveTab(7 - 1) },
-	{ key = '7', mods = 'LEADER',       action = act.ActivateTab(7 - 1) },
-	{ key = '8', mods = 'LEADER',       action = act.MoveTab(8 - 1) },
-	{ key = '8', mods = 'LEADER',       action = act.ActivateTab(8 - 1) },
-	{ key = '9', mods = 'LEADER',       action = act.MoveTab(9 - 1) },
-	{ key = '9', mods = 'LEADER',       action = act.ActivateTab(9 - 1) },
-	{ key = 'p', mods = 'LEADER',       action = act.MoveTabRelative(-1) },
-	{ key = 'p', mods = 'LEADER',       action = act.ActivateTabRelative(-1) },
-	{ key = 'n', mods = 'LEADER',       action = act.MoveTabRelative(1) },
-	{ key = 'n', mods = 'LEADER',       action = act.ActivateTabRelative(1) },
-	-- Create/close tabs.
-	{ key = 'c', mods = 'LEADER',       action = act.SpawnTab 'CurrentPaneDomain' },
-	{ key = 'x', mods = 'LEADER|SHIFT', action = act.CloseCurrentTab { confirm = true } },
-	-- Copy/paste.
-	{ key = 'c', mods = 'SHIFT|CTRL',   action = act.CopyTo 'ClipboardAndPrimarySelection' },
-	{ key = 'v', mods = 'SHIFT|CTRL',   action = act.PasteFrom 'Clipboard' },
-	-- Launcher.
-	{ key = "s", mods = "LEADER|SHIFT", action = act.ShowLauncher },
-	-- Search.
-	{ key = '/', mods = 'LEADER',       action = act.Search 'CurrentSelectionOrEmptyString' },
-	-- Workspaces.
+  { key = '"', mods = 'LEADER|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+  { key = '%', mods = 'LEADER|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = 'x', mods = 'LEADER', action = act.CloseCurrentPane { confirm = true } },
+  -- Move between tabs.
+  { key = '1', mods = 'LEADER', action = act.MoveTab(1 - 1) },
+  { key = '1', mods = 'LEADER', action = act.ActivateTab(1 - 1) },
+  { key = '2', mods = 'LEADER', action = act.MoveTab(2 - 1) },
+  { key = '2', mods = 'LEADER', action = act.ActivateTab(2 - 1) },
+  { key = '3', mods = 'LEADER', action = act.MoveTab(3 - 1) },
+  { key = '3', mods = 'LEADER', action = act.ActivateTab(3 - 1) },
+  { key = '4', mods = 'LEADER', action = act.MoveTab(3 - 1) },
+  { key = '4', mods = 'LEADER', action = act.ActivateTab(4 - 1) },
+  { key = '5', mods = 'LEADER', action = act.MoveTab(5 - 1) },
+  { key = '5', mods = 'LEADER', action = act.ActivateTab(5 - 1) },
+  { key = '6', mods = 'LEADER', action = act.MoveTab(6 - 1) },
+  { key = '6', mods = 'LEADER', action = act.ActivateTab(6 - 1) },
+  { key = '7', mods = 'LEADER', action = act.MoveTab(7 - 1) },
+  { key = '7', mods = 'LEADER', action = act.ActivateTab(7 - 1) },
+  { key = '8', mods = 'LEADER', action = act.MoveTab(8 - 1) },
+  { key = '8', mods = 'LEADER', action = act.ActivateTab(8 - 1) },
+  { key = '9', mods = 'LEADER', action = act.MoveTab(9 - 1) },
+  { key = '9', mods = 'LEADER', action = act.ActivateTab(9 - 1) },
+  { key = 'p', mods = 'LEADER', action = act.MoveTabRelative(-1) },
+  { key = 'p', mods = 'LEADER', action = act.ActivateTabRelative(-1) },
+  { key = 'n', mods = 'LEADER', action = act.MoveTabRelative(1) },
+  { key = 'n', mods = 'LEADER', action = act.ActivateTabRelative(1) },
+  -- Create/close tabs.
+  { key = 'c', mods = 'LEADER', action = act.SpawnTab 'CurrentPaneDomain' },
+  { key = 'x', mods = 'LEADER|SHIFT', action = act.CloseCurrentTab { confirm = true } },
+  -- Copy/paste.
+  { key = 'c', mods = 'SHIFT|CTRL', action = act.CopyTo 'ClipboardAndPrimarySelection' },
+  { key = 'v', mods = 'SHIFT|CTRL', action = act.PasteFrom 'Clipboard' },
+  -- Launcher.
+  { key = 's', mods = 'LEADER|SHIFT', action = act.ShowLauncher },
+  -- Search.
+  { key = '/', mods = 'LEADER', action = act.Search 'CurrentSelectionOrEmptyString' },
+  -- Workspaces.
   {
     key = 'w',
     mods = 'LEADER',
@@ -165,7 +164,7 @@ config.keys = {
     key = 'f',
     mods = 'LEADER',
     -- Asks to select a workspace from a list of pre-defined and already existing workspaces.
-    -- First, if the selected workspace does not already exist, spawn a window in it with 
+    -- First, if the selected workspace does not already exist, spawn a window in it with
     -- a specifed cwd. Then, switch to the workspace.
     action = act.InputSelector {
       title = 'Select workspace',
@@ -174,7 +173,7 @@ config.keys = {
 
         -- Add existing workspaces to the list of choices.
         for _, name in pairs(mux.get_workspace_names()) do
-          table.insert(choices, {label = name, id = name })
+          table.insert(choices, { label = name, id = name })
         end
 
         -- Keep track of added workspaces.
@@ -186,16 +185,16 @@ config.keys = {
         -- Add ~/.dotfiles to list of workspaces if not already exists.
         local dotfiles_label = '~/.dotfiles'
         if added[dotfiles_label] == nil then
-          table.insert(choices, {label = dotfiles_label, id = wezterm.home_dir .. '/.dotfiles' })
+          table.insert(choices, { label = dotfiles_label, id = wezterm.home_dir .. '/.dotfiles' })
           added[dotfiles_label] = true
         end
 
         -- Add all directories in ~/Projects to list of workspace if not already exists.
         local projects_dir = wezterm.home_dir .. '/Projects'
         for _, name in pairs(scandir(projects_dir)) do
-          local label = "~/Projects/" .. name
+          local label = '~/Projects/' .. name
           if added[label] == nil then
-            table.insert(choices, {label = label, id = projects_dir .. "/" .. name })
+            table.insert(choices, { label = label, id = projects_dir .. '/' .. name })
             added[label] = true
           end
         end
@@ -203,14 +202,16 @@ config.keys = {
         -- Add all directories in ~/Work to list of workspace if not already exists.
         local work_dir = wezterm.home_dir .. '/Work'
         for _, name in pairs(scandir(work_dir)) do
-          local label = "~/Work/" .. name
+          local label = '~/Work/' .. name
           if added[label] == nil then
-            table.insert(choices, {label = label, id = work_dir .. "/" .. name })
+            table.insert(choices, { label = label, id = work_dir .. '/' .. name })
             added[label] = true
           end
         end
 
-        table.sort(choices, function(a, b) return a.label < b.label end)
+        table.sort(choices, function(a, b)
+          return a.label < b.label
+        end)
 
         return choices
       end)(),
@@ -228,7 +229,7 @@ config.keys = {
         end
 
         if not workspace_exists then
-          mux.spawn_window { workspace = label, cwd = id } 
+          mux.spawn_window { workspace = label, cwd = id }
         end
 
         window:perform_action(
@@ -244,22 +245,21 @@ config.keys = {
   { key = 'p', mods = 'LEADER|SHIFT', action = act.SwitchWorkspaceRelative(-1) },
 }
 config.key_tables = {
-	search_mode = {
-		{ key = 'Escape',   mods = 'NONE', action = act.CopyMode 'Close' },
-		{ key = 'n',        mods = 'CTRL', action = act.CopyMode 'NextMatch' },
-		{ key = 'p',        mods = 'CTRL', action = act.CopyMode 'PriorMatch' },
-		{ key = 'r',        mods = 'CTRL', action = act.CopyMode 'CycleMatchType' },
-		{ key = 'u',        mods = 'CTRL', action = act.CopyMode 'ClearPattern' },
-		{ key = 'PageUp',   mods = 'NONE', action = act.CopyMode 'PriorMatchPage' },
-		{ key = 'PageDown', mods = 'NONE', action = act.CopyMode 'NextMatchPage' },
-	},
+  search_mode = {
+    { key = 'Escape', mods = 'NONE', action = act.CopyMode 'Close' },
+    { key = 'n', mods = 'CTRL', action = act.CopyMode 'NextMatch' },
+    { key = 'p', mods = 'CTRL', action = act.CopyMode 'PriorMatch' },
+    { key = 'r', mods = 'CTRL', action = act.CopyMode 'CycleMatchType' },
+    { key = 'u', mods = 'CTRL', action = act.CopyMode 'ClearPattern' },
+    { key = 'PageUp', mods = 'NONE', action = act.CopyMode 'PriorMatchPage' },
+    { key = 'PageDown', mods = 'NONE', action = act.CopyMode 'NextMatchPage' },
+  },
 }
 
 ------------------------------------
 -- General
 ------------------------------------
 config.automatically_reload_config = true
-config.window_close_confirmation = "NeverPrompt"
+config.window_close_confirmation = 'NeverPrompt'
 
 return config
-
