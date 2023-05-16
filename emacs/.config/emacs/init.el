6c0d9b7d63f81ba5ed39204375e7ea92cf6d3e42
Johan Rönkkö <johan.ronkko@formulate.app>
2023-05-16 21:28:30 +0200

    Update emacs config.

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -32,16 +32,12 @@
              :config
              (evil-collection-init))
 
+;;; Vertical completion UI based on the default completion system
+(use-package vertico
+             :init
+             (vertico-mode))
 
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(package-selected-packages '(evil-collection evil use-package)))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  )
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+             :init
+             (savehist-mode))
